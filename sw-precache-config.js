3f0bb6f6246ea0fb796986b1265a89cb6dd05f52
Jorge Linhares <jorgehclinhares@gmail.com>
2017-10-25 16:26:58 -0300

    feat: implementando correções para aprovação Lighthouse

---
+++ sw-precache-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  navigateFallback: '/index.html',
+  stripPrefix: 'dist',
+  root: 'dist/',
+  staticFileGlobs: [
+    'dist/index.html',
+    'dist/**.js',
+    'dist/**.css',
+    'dist/assets/**',
+    'dist/**.ttf',
+    'dist/**.woff',
+    'dist/**.svg',
+    'dist/**.eot',
+    'dist/**.json'
+  ]
+};
